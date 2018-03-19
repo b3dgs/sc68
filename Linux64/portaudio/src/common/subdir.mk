5fdd3fcaed1e65f33d1dc42f38086cfc2720cdd4
DjThunder <contact@b3dgs.com>
2018-03-19 21:42:09 +0100

    Makefile updated.

--- Linux64/portaudio/src/common/subdir.mk
+++ Linux64/portaudio/src/common/subdir.mk
@@ -44,7 +44,7 @@ C_DEPS += \
 portaudio/src/common/%.o: ../portaudio/src/common/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -DHAVE_CONFIG_H -D_GLIBCXX_USE_CXX11_ABI=0 -DPA_USE_ALSA=1 -I"/home/djthunder/eclipse-workspace/sc68/zlib" -I"/home/djthunder/eclipse-workspace/sc68" -I"/home/djthunder/eclipse-workspace/sc68/api68" -I"/home/djthunder/eclipse-workspace/sc68/emu68" -I"/home/djthunder/eclipse-workspace/sc68/file68" -I"/home/djthunder/eclipse-workspace/sc68/io68" -I"/home/djthunder/eclipse-workspace/sc68/unice68" -I"/home/djthunder/eclipse-workspace/sc68/portaudio/src/os/unix" -I"/home/djthunder/eclipse-workspace/sc68/portaudio/include" -I"/home/djthunder/eclipse-workspace/sc68/portaudio/src/common" -O3 -c -fmessage-length=0 -Wno-multichar -Wno-implicit-function-declaration -Wno-aggressive-loop-optimizations -Wno-deprecated-declarations -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -DHAVE_CONFIG_H -D_GLIBCXX_USE_CXX11_ABI=0 -DPA_USE_ALSA=1 -I"$(abspath $(CURDIR)/../zlib") -I"$(abspath $(CURDIR)/..") -I"$(abspath $(CURDIR)/../api68") -I"$(abspath $(CURDIR)/../emu68") -I"$(abspath $(CURDIR)/../file68") -I"$(abspath $(CURDIR)/../io68") -I"$(abspath $(CURDIR)/../unice68") -I"$(abspath $(CURDIR)/../portaudio/src/os/unix") -I"$(abspath $(CURDIR)/../portaudio/include") -I"$(abspath $(CURDIR)/../portaudio/src/common") -O3 -c -fmessage-length=0 -Wno-multichar -Wno-implicit-function-declaration -Wno-aggressive-loop-optimizations -Wno-deprecated-declarations -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Linux64/subdir.mk
+++ Linux64/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -DHAVE_CONFIG_H -D_GLIBCXX_USE_CXX11_ABI=0 -DPA_USE_ALSA=1 -I"$(abspath $(CURDIR)/../zlib)" -I"$(abspath $(CURDIR)/../)" -I"$(abspath $(CURDIR)/../api68)" -I"$(abspath $(CURDIR)/../emu68)" -I"$(abspath $(CURDIR)/../file68)" -I"$(abspath $(CURDIR)/../io68)" -I"$(abspath $(CURDIR)/../unice68)" -I"$(abspath $(CURDIR)/../portaudio/src/os/unix)" -I"$(abspath $(CURDIR)/../portaudio/include)" -I"$(abspath $(CURDIR)/../portaudio/src/common)" -O3 -c -fmessage-length=0 -Wno-multichar -Wno-implicit-function-declaration -Wno-aggressive-loop-optimizations -Wno-deprecated-declarations -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -DHAVE_CONFIG_H -D_GLIBCXX_USE_CXX11_ABI=0 -DPA_USE_ALSA=1 -I"$(abspath $(CURDIR)/../zlib)" -I"$(abspath $(CURDIR)/..)" -I"$(abspath $(CURDIR)/../api68)" -I"$(abspath $(CURDIR)/../emu68)" -I"$(abspath $(CURDIR)/../file68)" -I"$(abspath $(CURDIR)/../io68)" -I"$(abspath $(CURDIR)/../unice68)" -I"$(abspath $(CURDIR)/../portaudio/src/os/unix)" -I"$(abspath $(CURDIR)/../portaudio/include)" -I"$(abspath $(CURDIR)/../portaudio/src/common)" -O3 -c -fmessage-length=0 -Wno-multichar -Wno-implicit-function-declaration -Wno-aggressive-loop-optimizations -Wno-deprecated-declarations -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
